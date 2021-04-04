dc72ed4265fdb3c9981133ed5870391c5f615627
Shringiraj <anup6510@gmail.com>
2021-04-04 14:23:37 +0530

    init

---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+let username;
+let socket = io();
+do {
+    username = prompt('Please provide username.')
+} while (!username)
+
+let commentBox = document.querySelector('#commentBox')
+let postBtn = document.querySelector('#postBtn')
+let comments = document.querySelector('#comments')
+
+postBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    //alert(commentBox.value);
+
+    postComment(commentBox.value)
+})
+
+function postComment(comment) {
+    if (!comment) {
+        return;
+    }
+    data = {
+        comment,
+        username,
+    }
+    appendComment(data);
+    // Broadcast
+    broadcastComment(data);
+}
+
+function appendComment(data) {
+    let lTag = document.createElement('li');
+
+    let markup = `
+        <div class="card card-primary">
+            <div class="card-body m-0 py-1">
+                <p class="h6">${data.username}</p>
+                <p class="m-0">${data.comment}</p>
+                <p class="m-0">${moment(data.time).format('LT')}</p>
+            </div>
+        </div>
+    `
+    lTag.innerHTML = markup;
+    console.log(lTag)
+    comments.prepend(lTag)
+    commentBox.value = '';
+}
+function broadcastComment(data) {
+    socket.emit('comment',data);
+}
+
+socket.on('comment',(data)=>{
+    appendComment(data);
+})
